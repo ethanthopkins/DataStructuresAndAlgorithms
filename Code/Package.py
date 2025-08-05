1c882b5e576ad7c4e8af816e77bf93697373d09f
Ethan <ethanthopkins@gmail.com>
2025-08-05 13:08:11 -0600

    Adds HashTable class and constructor for package

--- Code/Package.py
+++ Code/Package.py
@@ -8,3 +8,12 @@ class Package:
     sKilo = 0
     notes = ""
 
+    def __init__(self, packageID, address, city, state, zip, deliveryDeadline, sKilo, notes):
+        self.packageID = packageID
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.deliveryDeadline = deliveryDeadline
+        self.sKilo = sKilo
+        self.notes = notes
