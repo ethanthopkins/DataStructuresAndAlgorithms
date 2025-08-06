9ee658b1506f6ffb9f80c9b8c04b9132b6de4055
Ethan <ethanthopkins@gmail.com>
2025-08-06 14:05:26 -0600

    fixes bug with importing wrong hashtable library

--- Code/Main.py
+++ Code/Main.py
@@ -3,6 +3,5 @@ from MyHashTable import MyHashTable
 
 package1 = Package(1, "195 W Oakland Ave", "Salt Lake City", "UT", 84115, "10:30AM", 21, "")
 hashTable = MyHashTable()
-#hashTable.insert(1, package1)
-#print(package1.packageID)
-#print(hashTable.search(1))
+hashTable.insert(1, package1)
+print(hashTable.search(1).address)
