3b72f89782c9b955b5ba652c76f52f42962fa416
Ethan <ethanthopkins@gmail.com>
2025-08-07 07:41:47 -0600

    starts code for csv reader

--- Code/Main.py
+++ Code/Main.py
@@ -3,7 +3,10 @@ from MyHashTable import MyHashTable
 import csv
 
 #parse csv
-#with open
+with open('../Materials/package_list.csv') as packageListCSV:
+    readerOBJ = csv.reader(packageListCSV)
+    for row in readerOBJ:
+        print(row)
 
 package1 = Package(1, "195 W Oakland Ave", "Salt Lake City", "UT", 84115, "10:30AM", 21, "")
 hashTable = MyHashTable()
