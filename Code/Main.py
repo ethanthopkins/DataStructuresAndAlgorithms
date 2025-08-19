b89dff7be8b414bc6cf6b58a3c0ae23800f048a1
Ethan <72623865+ethanthopkins@users.noreply.github.com>
2025-08-19 13:59:41 -0600

    Starts fulfilling task G. Needs to exclude last entry to include for back to hub time.

--- Code/Main.py
+++ Code/Main.py
@@ -31,42 +31,59 @@ timeCompEnd1 = datetime.combine(datetime.today(), time(9, 25))
 timeCompStart2 = datetime.combine(datetime.today(), time(9, 35))
 timeCompEnd2 = datetime.combine(datetime.today(), time(10, 25))
 timeCompStart3 = datetime.combine(datetime.today(), time(12, 3))
-timeCompEnd3 = datetime.combine(datetime.today(), time(1, 12))
+timeCompEnd3 = datetime.combine(datetime.today(), time(13, 12))
 
 #Task G (compare times)
 for package1 in truck1.getPackages():
     if ((package1[1] >= timeCompStart1) and (package1[1] <= timeCompEnd1)):
-        comp1.append(package1[0])
+        comp1.append(["Delivered", package1[0]])
+    else: comp1.append(["En Route", package1[0]])
     if ((package1[1] >= timeCompStart2) and (package1[1] <= timeCompEnd2)):
-        comp2.append(package1[0])
+        comp2.append(["Delivered", package1[0]])
+    else: comp2.append(["En Route", package1[0]])
     if ((package1[1] >= timeCompStart3) and (package1[1] <= timeCompEnd3)):
-        comp3.append(package1[0])
+        comp3.append(["Delivered", package1[0]])
+    else: comp3.append(["En Route", package1[0]])
 for package2 in truck2.getPackages():
-    if ((package2[1] >= timeCompStart1) and (package2[1] <= timeCompEnd1)):
-        comp1.append(package1[0])
-    if ((package2[1] >= timeCompStart2) and (package2[1] <= timeCompEnd2)):
-        comp2.append(package1[0])
-    if ((package2[1] >= timeCompStart3) and (package2[1] <= timeCompEnd3)):
-        comp3.append(package2[0])
+    comp1.append(["At the hub", package2[0]])
+    comp2.append(["At the hub", package2[0]])
+    comp3.append(["At the hub", package2[0]])
 for package3 in truck3.getPackages():
     if ((package3[1] >= timeCompStart1) and (package3[1] <= timeCompEnd1)):
-        comp1.append(package1[0])
+        comp1.append(["Delivered", package1[0]])
+    else:
+        comp1.append(["En Route", package3[0]])
     if ((package3[1] >= timeCompStart2) and (package3[1] <= timeCompEnd2)):
-        comp2.append(package1[0])
+        comp2.append(["Delivered", package1[0]])
+    else:
+        comp2.append(["En Route", package3[0]])
     if ((package3[1] >= timeCompStart3) and (package3[1] <= timeCompEnd3)):
-        comp3.append(package3[0])
+        comp3.append(["Delivered", package3[0]])
+    else:
+        comp3.append(["En Route", package3[0]])
 
+print("G1: ")
+for index1, status1 in enumerate(comp1):
+    print(index1, status1)
+print("")
+print("G2:")
+for index2, status2 in enumerate(comp2):
+    print(index2, status2)
+print("")
+print("G3")
+for index3, status3 in enumerate(comp3):
+    print(index3, status3)
 
 #test
-print("truck 1: " + str(truck1.totalMileage))
-for element1 in truck1.getPackages():
-    print("ID: " + str(element1[0].getPackageID()) + " Address: " + element1[0].getAddress() + " TIME: " + str(element1[1].time()))
-print("")
-print("truck 2: " + str(truck2.totalMileage))
-for element2 in truck2.getPackages():
-    print("ID: " + str(element2[0].getPackageID()) + " Address: " + element2[0].getAddress() + " TIME: " + str(element2[1].time()))
-print("")
-print("truck 3: " + str(truck3.totalMileage))
-for element3 in truck3.getPackages():    
-    print("ID: " + str(element3[0].getPackageID()) + " Address: " + element3[0].getAddress() + " TIME: " + str(element3[1].time()))
+#print("truck 1: " + str(truck1.totalMileage))
+#for element1 in truck1.getPackages():
+#    print("ID: " + str(element1[0].getPackageID()) + " Address: " + element1[0].getAddress() + " TIME: " + str(element1[1].time()))
+#print("")
+#print("truck 2: " + str(truck2.totalMileage))
+#for element2 in truck2.getPackages():
+#    print("ID: " + str(element2[0].getPackageID()) + " Address: " + element2[0].getAddress() + " TIME: " + str(element2[1].time()))
+#print("")
+#print("truck 3: " + str(truck3.totalMileage))
+#for element3 in truck3.getPackages():    
+#    print("ID: " + str(element3[0].getPackageID()) + " Address: " + element3[0].getAddress() + " TIME: " + str(element3[1].time()))
 
