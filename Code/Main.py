441b7087624e14563f12014e2f812c2ccc05e57d
= <=>
2025-08-22 23:28:15 -0600

    task g fixes

--- Code/Main.py
+++ Code/Main.py
@@ -24,6 +24,17 @@ class Main:
         self.truck2.setCurrentTime(self.truck1.getCurrentTime())
         self.truck2.loadPackages(self.parsing.getPackageLength(), self.parsing.getPackageHT(), self.parsing.getDistanceHT(), self.parsing.getAddressList())
 
+    def makeAddressCorrect(self, package, bool):
+        if (bool):
+            package.setAddress("410 S State St")
+            package.setCity("Salt Lake City")
+            package.setState("UT")
+            package.setZip(84111)  
+        else:
+            package.setAddress("300 State St")
+            package.setCity("Salt Lake City")
+            package.setState("UT")
+            package.setZip(84103)          
     def totalMileage(self):
         print("Truck 1 Total Mileage: ", self.truck1.getTotalMileage())
         print("Truck 2 Total Mileage: ", self.truck2.getTotalMileage())
@@ -48,61 +59,44 @@ class Main:
         for package1 in self.truck1.getPackages():
             if (package1[0].getPackageID() != 0):
                 if ((package1[1] >= timeCompStart1) and (package1[1] <= timeCompEnd1)):
+                    comp1.append(["Delivered: ", package1])
+                else: 
                     #this handles the exception for package 9
-                    if ((package1[0].getPackageID() == 9) and (endTime < time(10, 20))):
-                        package1[0].setAddress("300 State St")
-                        package1[0].setCity("Salt Lake City")
-                        package1[0].setState("UT")
-                        package1[0].setZip(84103)
-                    else:
-                        package1[0].setAddress("410 S State St")
-                        package1[0].setCity("Salt Lake City")
-                        package1[0].setState("UT")
-                        package1[0].setZip(84111)
-                    comp1.append(["Delivered", package1[0]])
-                else: comp1.append(["En Route", package1[0]])
+                    if (package1[0].getPackageID() == 9): 
+                        if (timeCompEnd1 < datetime.combine(datetime.today(), time(10, 20))):
+                            self.makeAddressCorrect(package1[0], False)
+                            comp1.append(["Delayed", package1])
+                            continue
+                        else:
+                            self.makeAddressCorrect(package1[0], True)
+                    comp1.append(["En Route", package1])
         for package2 in self.truck2.getPackages():
+            if (timeCompEnd1 < datetime.combine(datetime.today(), time(9, 5))):
+                if (package2[0].getPackageID() in [6, 25, 28, 32]):
+                    comp1.append(["Delayed", package2])
+                    continue
             if (package2[0].getPackageID() != 0):
-                #this handles the exception for package 9
-                if ((package1[0].getPackageID() == 9) and (endTime < time(10, 20))):
-                    package1[0].setAddress("300 State St")
-                    package1[0].setCity("Salt Lake City")
-                    package1[0].setState("UT")
-                    package1[0].setZip(84103)
-                else:
-                    package1[0].setAddress("410 S State St")
-                    package1[0].setCity("Salt Lake City")
-                    package1[0].setState("UT")
-                    package1[0].setZip(84111)
-                comp1.append(["At the hub", package2[0]])
+                comp1.append(["At the hub", package2])
         for package3 in self.truck3.getPackages():
             if (package3[0].getPackageID() != 0):
                 if ((package3[1] >= timeCompStart1) and (package3[1] <= timeCompEnd1)):
-                    #this handles the exception for package 9
-                    if ((package1[0].getPackageID() == 9) and (endTime < time(10, 20))):
-                        package1[0].setAddress("300 State St")
-                        package1[0].setCity("Salt Lake City")
-                        package1[0].setState("UT")
-                        package1[0].setZip(84103)
-                    else:
-                        package1[0].setAddress("410 S State St")
-                        package1[0].setCity("Salt Lake City")
-                        package1[0].setState("UT")
-                        package1[0].setZip(84111)
-                    comp1.append(["Delivered", package3[0]])
-                else: comp1.append(["En Route", package3[0]])
+                    comp1.append(["Delivered", package3])
+                else: comp1.append(["En Route", package3])
         
         print("Package Status ")
         for index1, status1 in enumerate(comp1):
-            print("Status: ", status1[0])
-            print("PACKAGE ID:", status1[1].getPackageID(), 
-                "ADDRESS:", status1[1].getAddress(), 
-                "CITY:", status1[1].getCity(),
-                "STATE:", status1[1].getState(),
-                "ZIP", status1[1].getZip(),
-                "DELIVERY DEADLINE:", status1[1].getDeliveryDeadline(),
-                "SKILO:", status1[1].getSKilo(),
-                "NOTES:", status1[1].getNotes())
+            if (status1[0] == "Delivered"):
+                print("Status: ", status1[0], ": ", status1[1][1].time())
+            else:
+                print("Status: ", status1[0])    
+            print("PACKAGE ID:", status1[1][0].getPackageID(), 
+                "ADDRESS:", status1[1][0].getAddress(), 
+                "CITY:", status1[1][0].getCity(),
+                "STATE:", status1[1][0].getState(),
+                "ZIP", status1[1][0].getZip(),
+                "DELIVERY DEADLINE:", status1[1][0].getDeliveryDeadline(),
+                "SKILO:", status1[1][0].getSKilo(),
+                "NOTES:", status1[1][0].getNotes())
             
 main = Main()
 #main.totalMileage()
